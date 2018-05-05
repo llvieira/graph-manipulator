21eed50764b03f7ac9ce9a108363058394868933
GustavoDinizMonteiro <gustavo.monteiro@ccc.ufcg.edu.br>
2018-05-05 21:33:46 +0000

    Add implementation for readGraph method.

--- src/main/java/manipulator/GraphManipulator.java
+++ src/main/java/manipulator/GraphManipulator.java
@@ -1,59 +1,108 @@
 package manipulator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+
 public class GraphManipulator implements Manipulator {
+	private Map<Integer, Set<Integer>> graph;
+	
+	public GraphManipulator() {
+		this.graph = new HashMap<>();
+	}
 
-    @Override
-    public void readGraph(String path) {
+	@Override
+	public void readGraph(String path) {
+		try {
+			Map<Integer, Integer> connections = this.getValuesFromFile(path);
+			
+			for(Integer vertex : connections.keySet()) {
+				if(this.graph.containsKey(vertex)) {
+					this.graph.get(vertex).add(connections.get(vertex));
+				}else {
+					this.graph.put(vertex, new HashSet<>());
+					this.graph.get(vertex).add(vertex);
+				}
+			}
+			
+		} catch (FileNotFoundException e) {
+			System.out.println("File" + path + " does not exists.");
+		}
+	}
 
-    }
+	@Override
+	public void readWeightedGraph(String path) {
 
-    @Override
-    public void readWeightedGraph(String path) {
+	}
 
-    }
+	@Override
+	public int getVertexNumber(Graph graph) {
+		return 0;
+	}
 
-    @Override
-    public int getVertexNumber(Graph graph) {
-        return 0;
-    }
+	@Override
+	public int getEdgeNumber(Graph graph) {
+		return 0;
+	}
 
-    @Override
-    public int getEdgeNumber(Graph graph) {
-        return 0;
-    }
+	@Override
+	public float getMeanEdge(Graph graph) {
+		return 0;
+	}
 
-    @Override
-    public float getMeanEdge(Graph graph) {
-        return 0;
-    }
+	@Override
+	public String graphRepresentation(Graph graph, String type) {
+		return null;
+	}
 
-    @Override
-    public String graphRepresentation(Graph graph, String type) {
-        return null;
-    }
+	@Override
+	public String BFS(Graph graph, Vertex vertex) {
+		return null;
+	}
 
-    @Override
-    public String BFS(Graph graph, Vertex vertex) {
-        return null;
-    }
+	@Override
+	public String DFS(Graph graph, Vertex vertex) {
+		return null;
+	}
 
-    @Override
-    public String DFS(Graph graph, Vertex vertex) {
-        return null;
-    }
+	@Override
+	public String SCC(Graph graph) {
+		return null;
+	}
 
-    @Override
-    public String SCC(Graph graph) {
-        return null;
-    }
+	@Override
+	public String shortestPath(Vertex vertex1, Vertex vertex2) {
+		return null;
+	}
 
-    @Override
-    public String shortestPath(Vertex vertex1, Vertex vertex2) {
-        return null;
-    }
+	@Override
+	public String MST(Graph graph) {
+		return null;
+	}
 
-    @Override
-    public String MST(Graph graph) {
-        return null;
-    }
+	private Map<Integer, Integer> getValuesFromFile(String path) throws FileNotFoundException {
+		File file = new File(path);
+		Scanner scanner = new Scanner(file);
+
+		Map<Integer, Integer> mapOfConnections = new HashMap<>();
+
+		while (scanner.hasNextLine()) {
+			String line = scanner.nextLine();
+			String[] values = line.trim().split(" ");
+
+			if (values.length > 1) {
+				mapOfConnections.put(Integer.valueOf(values[0]), Integer.valueOf(values[1]));
+			} else {
+				mapOfConnections.put(Integer.valueOf(values[0]), null);
+			}
+		}
+
+		scanner.close();
+
+		return mapOfConnections;
+	}
 }
