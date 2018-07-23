dace2b2ced1cd8b99fa08edb85937d1b6f575f5d
Gustavo Monteiro <dinizmonteirogustavo@gmail.com>
2018-07-23 16:58:57 -0300

    Add base algoritm for shortest path.

--- src/main/java/manipulator/Main.java
+++ src/main/java/manipulator/Main.java
@@ -1,13 +1,56 @@
 package manipulator;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.stream.Collectors;
+
 public class Main {
 
 	public static void main(String[] args) {
 		GraphManipulator graphManipulator = new GraphManipulator();
 		String path = "src/test/java/manipulator/samples/graph-sample-with-weight.txt";
+
+		Graph<String> graph = graphManipulator.readWeightedGraph(path);
 		
-		graphManipulator.readWeightedGraph(path);
+		Vertex<String> tomCruize =  graph.getNodes().stream()
+				.filter(node -> node.getValue().equals("Tom Cruise"))
+				.findFirst().get();
 		
+		List<Vertex<String>> otherActors = graph.getNodes().stream()
+					.filter(node -> !node.getValue().equals("Tom Cruise"))
+					.collect(Collectors.toList());
 		
+	}
+	
+	public static void shortestPath(List<Vertex<String>> otherActors, Vertex<String> target) {
+		Map<String, Integer> distances = new HashMap<>();
+		for (Vertex<String> vertex : otherActors) {
+			Queue<Vertex<String>> queue = new LinkedList<>();
+			
+			for(Edge<String> e : vertex.getEdges()) {
+				queue.add(e.getEnd()); // Need fix, should have the initial vertex.
+			}
+			
+			Queue<Vertex<String>> aux = new LinkedList<>();
+			int level = 1;
+			
+			while(queue.size() > 0) {
+				for (Vertex<String> v : queue) {
+					if (v.equals(target)) {
+						distances.put(v.getValue(), level);
+						break;
+					}
+					for(Edge<String> e2: v.getEdges()) {
+						aux.add(e2.getEnd());
+					}
+				}
+				queue = aux;
+				aux = new LinkedList<>();
+				level++;
+			}
+		}
 	}
 }
